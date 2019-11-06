aabf042318b5b92929128e0bd9233b8b410ee980
Peter019 <46199411+Peter019@users.noreply.github.com>
2019-11-06 21:10:45 +0100

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,19 @@
 const express = require('express');
-const user_profile = require('../controllers/user')
+const { allUsers,getUser,updateUser,deleteUser,userPic } = require('../controllers/user')
 const router = express.Router();
-const signinController = require('../controllers/auth');
-
+const { requireSignin } = require('../controllers/auth');
+const { addFollowing,addFollower } = require('../controllers/user');
 
 //for any req containing userId
 router.param("userId" , user_profile.userById);
 
-router.get("/users",signinController.requireSignin,user_profile.allUsers);
-router.get("/user/:userId",signinController.requireSignin,user_profile.getUser);
-router.put("/user/:userId",signinController.requireSignin,user_profile.updateUser);
-router.delete("/user/:userId",signinController.requireSignin,user_profile.deleteUser);
+router.get("/users",requireSignin,allUsers);
+router.get("/user/:userId",requireSignin,getUser);
+router.put("/user/:userId",requireSignin,updateUser);
+router.delete("/user/:userId",requireSignin,deleteUser);
 //pic
-router.get("/user/picture/:userId",user_profile.userPic);
+router.get("/user/picture/:userId",userPic);
+//following
+router.put('/user/follow',requireSignin,requireSignin,addFollower,addFollowing)
 
 module.exports = router;
