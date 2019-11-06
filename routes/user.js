7d6a81ef849efb28679d8ad28f46fbd38da7b9da
Peter019 <46199411+Peter019@users.noreply.github.com>
2019-11-06 21:17:30 +0100

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { allUsers,getUser,updateUser,deleteUser,userPic } = require('../controllers/user')
+const { allUsers,getUser,updateUser,deleteUser,userPic,userById } = require('../controllers/user')
 const router = express.Router();
 const { requireSignin } = require('../controllers/auth');
 const { addFollowing,addFollower } = require('../controllers/user');
 
 //for any req containing userId
-router.param("userId" , user_profile.userById);
+router.param("userId" , userById);
 
 router.get("/users",requireSignin,allUsers);
 router.get("/user/:userId",requireSignin,getUser);
