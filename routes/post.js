38a750bae926d8e950034aab560651c323e14919
Peter019 <46199411+Peter019@users.noreply.github.com>
2019-11-06 21:11:18 +0100

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -1,22 +1,22 @@
-const postController = require('../controllers/post');
+const { getPosts,createPost,postsByUser,updatePost,deletePost} = require('../controllers/post');
 const express = require('express');
-const validatePost = require('../validate/valid');
+const { createPostValidator } = require('../validate/valid');
 const router = express.Router();
-const signinController = require('../controllers/auth');
-const user_profile = require('../controllers/user')
+const { requireSignin } = require('../controllers/auth');
+const { userById } = require('../controllers/user');
 
 
 //for any req containing userId
-router.param("userId" , user_profile.userById);
+router.param("userId" , userById);
 // -||- postId
-router.param("postId" , postController.postById);
+router.param("postId" , postById);
 
 
-router.get("/posts",signinController.requireSignin,postController.getPosts);
-router.post("/post/new/:userId",signinController.requireSignin,postController.createPost, validatePost.createPostValidator);
-router.get("/post/by/:userId",signinController.requireSignin,postController.postsByUser);
+router.get("/posts",requireSignin,getPosts);
+router.post("/post/new/:userId",requireSignin,createPost, createPostValidator);
+router.get("/post/by/:userId",requireSignin,postsByUser);
 
-router.put("/post/:postId",signinController.requireSignin,postController.isAuthor ,postController.updatePost)
-router.delete("/post/:postId",signinController.requireSignin,postController.isAuthor ,postController.deletePost)
+router.put("/post/:postId",requireSignin,isAuthor ,updatePost);
+router.delete("/post/:postId",requireSignin,isAuthor ,deletePost)
 
 module.exports = router;
