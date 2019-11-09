28e6731d43db1fee5068ee993ddeab271bea867a
Peter019 <46199411+Peter019@users.noreply.github.com>
2019-11-09 20:38:25 +0100

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,13 @@ const express = require('express');
 const { allUsers,getUser,updateUser,deleteUser,userPic,userById } = require('../controllers/user')
 const router = express.Router();
 const { requireSignin } = require('../controllers/auth');
-const { addFollowing,addFollower } = require('../controllers/user');
+const { addFollowing,addFollower,removeFollower,removeFollowing,findPeople } = require('../controllers/user');
 
 //for any req containing userId
 router.param("userId" , userById);
+//following
+router.put('/user/follow',requireSignin,requireSignin,addFollower,addFollowing)
+router.put('/user/unfollow',requireSignin,removeFollowing,removeFollower);
 
 router.get("/users",requireSignin,allUsers);
 router.get("/user/:userId",requireSignin,getUser);
@@ -13,7 +16,6 @@ router.put("/user/:userId",requireSignin,updateUser);
 router.delete("/user/:userId",requireSignin,deleteUser);
 //pic
 router.get("/user/picture/:userId",userPic);
-//following
-router.put('/user/follow',requireSignin,requireSignin,addFollower,addFollowing)
+router.get('/user/discover/:userId', requireSignin,findPeople)
 
 module.exports = router;
